a4efc97d602a6d891043c9da727991fc37ffd048
Raymond Maetha wa-Meida <raymond.maethawa-meida@student.ap.be>
2024-10-19 11:45:08 +0000

    render

---
+++ index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("port", 3000);
+
+// renderen pagina INDEX
+app.get("/",(req,res)=>{
+  res.render("index");
+})
+
+// renderen pagina Login
+app.get("/login",(req,res)=> {
+    res.render("login");
+})
+
+
+app.listen(app.get("port"), ()=>console.log( "[server] http://localhost:" + app.get("port")));
