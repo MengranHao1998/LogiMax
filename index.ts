03c38419fb10a935b415e112e6f57e4f1d50766a
Raymond Maetha wa-Meida <raymond.maethawa-meida@student.ap.be>
2024-10-20 14:22:31 +0000

    header-nav

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("port", 3000);
 
+app.use(express.static('public'))
+
 // renderen pagina INDEX
 app.get("/",(req,res)=>{
   res.render("index");
