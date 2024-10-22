c882e5f66611d3adb02f7b0f3a77c4fb6d5bb6eb
Sidney Wackenier <Sidneywackenier@hotmail.com>
2024-10-22 03:30:24 +0000

    Edited login.ejs (Added error message for incorrect login and changed values for login fields), Added simple login funcionality.

--- app.ts
+++ app.ts
@@ -5,14 +5,30 @@ app.set("view engine", "ejs");
 app.set("port", 3000);
 
 app.use(express.static("public/public"));
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended:true}))
 
 // renderen pagina LOGIN
-app.get("/login",(req,res)=>{
+app.get("/",(req,res)=>{
   res.render("login",{activePage: "login"});
 })
 
+// test user
+app.post('/', (req, res) => {
+  const { username, password } = req.body;
+
+  const correctUsername = 'JannesPeeters@Logimax.com';
+  const correctPassword = 'WPL';
+
+  if (username === correctUsername && password === correctPassword) {
+      res.redirect('/home');
+  } else {
+    res.render('login', { invalidCredentials: true });
+  }
+});
+
 // renderen pagina INDEX
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   res.render('index', { activePage: 'home' }); // activePage => voor gebruik nav item
 });
 
