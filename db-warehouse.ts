09e5015acf56a34f87b780d1163c6ca4db9d6879
Ponci Garcia <s080037@ap.be>
2024-11-17 19:27:04 +0000

    first working version

--- db-warehouse.ts
+++ db-warehouse.ts
@@ -1,7 +1,13 @@
 import { Employee, Order, Product, ProductPrice, Shipment, Warehouse } from "./types";
+import { MongoClient, Collection } from "mongodb";
+
+const MONGODB_URI = "mongodb+srv://db-admin:logimax@db-warehouses.uym2d.mongodb.net/?retryWrites=true&w=majority&appName=db-warehouses";
+const client = new MongoClient(MONGODB_URI);
+
+const warehousesCollection: Collection<Warehouse> = client.db("db-warehouses").collection<Warehouse>("warehouses");
 
 async function fetchDataWarehouses() {
-    const response = await fetch("https://logimax-api.onrender.com/warehouses/1", {
+    const response = await fetch("https://logimax-api.onrender.com/warehouses/", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -9,7 +15,33 @@ async function fetchDataWarehouses() {
         }
     });
     const data = await response.json();
-    console.log(data);
+
+    await warehousesCollection.deleteMany({}); // Telkens verwijderen voor testing's sake
+
+    await warehousesCollection.insertMany(data);
 }
 
-fetchDataWarehouses();
+async function DBConnect() {
+    try {
+        await client.connect();
+        console.log("Successfully connected to the database");
+        await fetchDataWarehouses();
+        console.log("Successfully wrote data to db")
+        process.on("SIGINT", DBExit); // Ctrl + C handling
+    } catch (e) {
+        console.error("Error connecting to the database:", e);
+    }
+}
+
+async function DBExit() {
+    try {
+        await client.close();
+        console.log("Disconnected from database");
+    } catch (error) {
+        console.error("Error while disconnecting from database:", error);
+    }
+    process.exit(0);
+}
+
+
+DBConnect();
